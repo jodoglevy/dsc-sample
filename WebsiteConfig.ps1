014a5405dbc6fb60cc7d71a0845a64a9462aa0e9
Joe Levy <jodoglevy@gmail.com>
2014-11-28 19:05:36 -0800

    adding dsc config

---
+++ WebsiteConfig.ps1
@@ -0,0 +1,12 @@
+Configuration WebsiteConfig
+{
+    Node ("localhost")
+    {
+        # Install the Web Server role
+        WindowsFeature IIS
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+        }
+    }
+}
